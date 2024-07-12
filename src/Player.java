dfd2cabd53a14f0d5352bc0655f1f478b8cdd402
oikawa ryouma <r.oikawa.sys23@morijyobi.ac.jp>
2024-07-12 13:03:04 +0900

    コンフリクトの解消

--- src/Player.java
+++ src/Player.java
@@ -12,3 +12,36 @@ public class Player {
         this.playerName = playerName;
     }
 }
+
+    private String playerName;
+
+    private Hand hand;
+
+
+    public Player(String playerName) {
+
+        this.playerName = playerName;
+
+    }
+
+    public String getPlayerName() {
+
+        return playerName;
+    }
+    public void setPlayerName(String playerName) {
+        this.playerName = playerName;
+    }
+
+
+
+    public Hand getHand() {
+        return this.hand;
+    }
+
+
+
+    public void setHand(Hand hand) {
+        this.hand = hand;
+    }
+
+}
